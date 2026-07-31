42ef9d892b0a4feff8f43f08e7516833312b0ca9
shreshthabanodha-lang <shreshthabanodha@gmail.com>
2026-08-01 01:00:01 +0530

    Time: 7 ms (86.96%), Space: 14 MB (83.78%) - LeetHub

--- 0016-3sum-closest/0016-3sum-closest.cpp
+++ 0016-3sum-closest/0016-3sum-closest.cpp
@@ -2,23 +2,36 @@ class Solution {
 public:
     int threeSumClosest(vector<int>& nums, int target) {
         
-        int closest = nums[0] + nums[1] + nums[2] ;
-       
-       for (int i = 0 ; i < nums.size() -2 ;i ++) {
-        
-        for (int j = i+1 ; j < nums.size() - 1 ; j ++) {
+        sort (nums.begin() , nums.end()) ;
+        long long closest = INT_MIN ;
+        for (int i = 0 ; i < nums.size() ; i ++) {
+            if (i>0 && nums[i] == nums[i-1] ) continue ;
+
+            int j = i+1 ;
+            int k = nums.size() -1 ;
+
+            while (j< k) {
             
-            for (int k = j+1;  k < nums.size() ; k ++){
-                
-                int sm = nums[i] + nums[j] + nums[k] ;
-                
-                if (abs(sm - target) < abs(closest - target)) {
-                    closest = sm ;
-                }
+                int sum = nums[i] + nums[j] + nums[k] ;
+
+                if (abs(target - sum) < abs(target - closest)) closest = sum ;
 
 
+                if (sum < target ) j++ ;
+                else if (sum > target) k--;
+
+                else return sum ;
+
+                while (j>k && nums[j] == nums[j-1]) j++ ;
+                while (j>k && nums[k] == nums[k+1]) k++ ;
+                
+                    
+
+                
             }
-        }
-       } return closest ;
+
+
+        } return closest ;
+
     }
 };
