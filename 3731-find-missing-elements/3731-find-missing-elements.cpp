7d6abd42b28636fb880e93f2fffc0a4b9b310f78
shreshthabanodha-lang <shreshthabanodha@gmail.com>
2026-08-05 05:02:29 +0530

    Time: 4 ms (35.29%), Space: 32.3 MB (54.68%) - LeetHub

--- 3731-find-missing-elements/3731-find-missing-elements.cpp
+++ 3731-find-missing-elements/3731-find-missing-elements.cpp
@@ -7,20 +7,25 @@ public:
 
         int n = nums.size() ;
 
-        unordered_map <int,int> hash ;
+       
 
-        for (int i = 0 ; i < n ; i ++) {
-            hash[nums[i]] ++ ;
+        for (int i = 1 ; i < n ; i ++) {
+
+            int pre = nums[i-1] ;
+            int cur = nums[i] ;
+
+            while (pre + 1 < cur) {
+
+                ans.push_back(pre+1) ;
+                pre++ ;
+
+
+            }   
+            
         }
 
-        for (int i = nums[0] ; i <= nums[n-1] ; i++) {
-
-            if (hash[i] == 0){
-
-                ans.push_back(i);
-
-            }
-         }
+        
+         
 
          return ans ;
 
